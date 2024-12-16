189597ab38386ec72f9bc52c4c5385932f9d4e45
juppo <xjuppo@gmail.com>
2024-12-16 02:40:48 +0100

    Update examples.py

--- src/examples.py
+++ src/examples.py
@@ -5,11 +5,13 @@ def plot_csv(path: str):
     # Read values from csv file and plot values
     values: dict = parser.read_from_file(path)
     x = values["time"]
-    gFx, = plt.plot(x, values["gFx"], label="gFx")
-    gFy, = plt.plot(x, values["gFy"], label="gFy")
-    gFz, = plt.plot(x, values["gFz"], label="gFz")
-    tgF, = plt.plot(x, values["TgF"], label="TgF")
-    plt.legend([gFx, gFy, gFz, tgF], ["gFx", "gFy", "gFz", "TgF"])
-    plt.show()
+    time, std_y = util.movstd(x, values["TgF"])
+    
+    plt.plot(x, values["TgF"], label="TgF")
+    plt.plot(time, std_y)
 
-plot_csv("samples/1.csv")
+    i1, i2 = util.find_fall_time(x, values["TgF"])
+    plt.axvline(x[i1], color = "r")
+    plt.axvline(x[i2], color = "r")
+
+    plt.show()
